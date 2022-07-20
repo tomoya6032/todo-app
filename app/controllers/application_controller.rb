c1f38286b97bcae9a0481be1c912ee5c6ed77d60
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-07-20 15:39:23 +0900

    sign-in-upのcssがほぼできたので、board一覧画面を作りに入る

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_article, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
 end
