eee8edb3b8fbd5b0d5be81e38be1c88d08cda7b9
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-07-27 06:53:10 +0900

    データベースのArgumentError: you can't define an already defined column 'user_id'.のエラーは解消されたので、今度は本題のuser_idを渡してboardが作成されるように試行錯誤する

--- db/migrate/20220721213624_create_boards.rb
+++ db/migrate/20220721213624_create_boards.rb
@@ -4,11 +4,8 @@ class CreateBoards < ActiveRecord::Migration[6.0]
       t.string :name, null: false
       t.text :description, null: false
       t.references :user, null: false, foreign_key: true
-      t.integer :user_id
       
       t.timestamps
     end
-    add_index :posts, :user_id
-    add_foreign_key :posts, :users
   end
 end
---
+++ db/migrate/20220726113213_add_user_id_to_boards.rb
@@ -0,0 +1,5 @@
+class AddUserIdToBoards < ActiveRecord::Migration[6.0]
+  def change
+    
+  end
+end
