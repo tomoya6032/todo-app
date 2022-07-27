10e6a84f285ce49f90653aca13c82b08450b14bc
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-07-28 06:55:21 +0900

    ターミナルからのデータ保存は諦めて記事一覧を作成していく。 記事一覧を作ってみたが、表示する記事がないので、記事作成画面を作っていく。 次回はday11CRUD機能を作ってみよう ー 記事の投稿①からやる

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,7 +4,7 @@ class Board < ApplicationRecord
   validates :name, format: { with: /\A(?!\@)/ }
 
   validates :description, presence: true
-  validates :description, length: { minimum: 10 }
+  validates :description, length: { minimum: 2 }
   validates :description, uniqueness: true
 
   validate :validate_name_and_description_length
@@ -12,8 +12,8 @@ class Board < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   
-  belongs_to :user
-  belongs_to :genre, optional: true
+  belongs_to :user , optional: true
+  # belongs_to :genre, optional: true
   
   def display_created_at
     I18n.l(self.created_at, format: :default)
