430c0eb8e40d71292fa08188843115566f4afcbd
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-08-04 06:54:31 +0900

    nameをtitleに変更しても保存ができずundefined method `validate_title_and_description_lengthというエラーが出てしまう。 明日はこのエラーをクリアする

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,13 +1,13 @@
 class Board < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { minimum: 2, maximum: 100  }
-  validates :name, format: { with: /\A(?!\@)/ }
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100  }
+  validates :title, format: { with: /\A(?!\@)/ }
 
   validates :description, presence: true
   validates :description, length: { minimum: 2 }
   validates :description, uniqueness: true
 
-  validate :validate_name_and_description_length
+  validate :validate_title_and_description_length
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
