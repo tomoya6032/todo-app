977d8f0f64cf65bb80675dd3dc29c0eb6a8c6902
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-08-11 06:30:20 +0900

    記事一覧から個別記事に飛べるようになった。 次は編集と削除をなどを作っていく

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,35 +1,36 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  def index
+ 
+   def index
     @boards = Board.all 
-  end
+   end
 
-  def show
+   def show
     @board = Board.find(params[:id])
-  end
+   end
 
-  def new
+   def new
     @board = Board.new
-  end
+   end
 
 
-  def create
-    @board = current_user.boards.build(board_params)
-    if @board.save
-      redirect_to boards_path(@board), notice: '保存ができたよ'
-    else
-      flash.now[:error] = '保存に失敗しました'
-      render :new
-    end
-  end
+   def create
+     @board = current_user.boards.build(board_params)
+     if @board.save
+       redirect_to boards_path(@board), notice: '保存ができたよ'
+     else
+       flash.now[:error] = '保存に失敗しました'
+       render :new
+     end
+   end
 
-  private
-  def board_params
-    params.require(:board).permit(:title, :description)
-  end
+   private
+   def board_params
+     params.require(:board).permit(:title, :description)
+   end
 
-  def set_board
-    @board = Board.find(params[:id])
-  end
+   def set_board
+     @board = Board.find(params[:id])
+   end
 end
