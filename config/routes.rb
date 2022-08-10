f4eecdf6da98832589d43353234a63219d69a412
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-08-10 17:39:43 +0900

    show画面の修正中。データはちゃんと保存されているからあとは取り出すだけ。 htmlの不具合か

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
    root to: "boards#index"
 
-  resources :boards, only: [:new, :create, :new]
+  resources :boards do
+    resources :comments, only: [:new, :create]
+
+    resource :like, only: [:create, :destroy]
+  end
+
+  resource :profile, only: [:show, :edit, :update]
+  resources :favorites, onle: [:index]
 end
+
