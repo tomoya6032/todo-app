89b00c490a45c22ddf2be364c190dc6e7f761fd2
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-08-29 07:15:39 +0900

    params[:board_id]あたりが怪しいのがわかったので、次回はスラックを見ながら変更していこう

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  
 
   def new
     board = Board.find(params[:board_id])
@@ -8,9 +9,9 @@ class TasksController < ApplicationController
 
   def create
     board = Task.find(params[:board_id])
-    @task = board.tasks.build(comment_params)
+    @task = board.tasks.build(task_params)
     if @task.save
-      redirect_to board_path(board), notice: 'タスクを追加' 
+      redirect_to board_tasks_path(board), notice: 'タスクを追加' 
     else
      flash.now[:error] = '更新できませんでした'
      render :new
@@ -19,7 +20,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:task)
+    params.require(:task).permit(:title, :content)
   end
 
 end
