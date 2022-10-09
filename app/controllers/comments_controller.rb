b75466e5236d363425c6fceaa4416fee2e23fab8
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-10-10 08:59:56 +0900

    昨日できなかったボードの配下のタスクににコメントをできるようになった。ただ、保存と表示ができるが、サブミットボタンを押してredirect toがうまくいかない。明日はここから

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,28 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:new, :create]
+  # before_action :set_comment, only: [:new, :create]
   before_action :authenticate_user!, only: [:new, :show, :create, :edit, :destroy]
  
 
    def show
-
+    
    end
   
    def new
-     board = Board.find(params[:board_id])
+    #  board = Board.find(params[:board_id])
      task = Task.find(params[:task_id])
+     @comment = current_user.comments.build
      @comment = task.comments.build
+     
    end
 
    def create
     #  task = current_user.tasks.find(params[:task_id])
      task = Task.find(params[:task_id]) 
     #  @comment = task.comments.build(comment_params.merge!(user_id: current_user.id))
-    #  @comment = task.comments.build(comment_params.merge!(user_id: current_user.id))
-     @comment = current_user.comments.build(comment_params)
+     @comment = task.comments.build(comment_params.merge!(user_id: current_user.id))
+    #  @comment = current_user.comments.build(comment_params)
      if @comment.save
-       redirect_to task_path(board), notice: 'コメントを追加'
+       redirect_to board_task_path(@task), notice: 'コメントを追加'
      else
        flash.now[:error] = '更新できませんでした'
        render :new
@@ -33,8 +35,7 @@ class CommentsController < ApplicationController
    end
 
   #  def set_comment
-  #    @comment = Comment.find(params[:task_id])
-  #   @task = Task.find(params[:id])
+  #    @comment = Comment.find(params[:id])
   #  end
 
 end
