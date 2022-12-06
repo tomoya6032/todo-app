7d7d1419076c24f1598d3cd7a4df2991c87c7d05
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-12-07 05:59:51 +0900

    今日もアイコン表示がうまくいかない

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,13 @@ class Comment < ApplicationRecord
   belongs_to :user
   has_one_attached :avatar
   validates :content,presence: true
+
+  # def avatar_image
+  #   if comment&.avatar&.attached?
+  #     comment.avatar
+  #   else
+  #     'default-avatar.png'
+  #   end
+  # end
+
 end
